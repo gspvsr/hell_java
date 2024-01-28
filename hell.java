447158df54117a1a94603277f8110c77c8cd85f0
gspvsr <153313380+gspvsr@users.noreply.github.com>
2024-01-27 21:11:13 -0600

    Update hell.java

--- hell.java
+++ hell.java
@@ -1,28 +1,7 @@
-import java.io.IOException;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+// Your First Program
 
-public class Add {
-
-public static void main(String[] args)throws IOException
-{
-    int a=0, b=0, sum;
-    Scanner sc=new Scanner(System.in);
-    System.out.println("Enter the numbers to be summed");
-    try{
-        a=sc.nextInt();
-        sc.nextLine();
-        b=sc.nextInt();
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
     }
-    catch(InputMismatchException e){
-        System.out.println("Please enter an Integer number");
-        e.printStackTrace();}
-    catch(Exception e){System.out.println(e);}
-
-    sum=a+b;
-
-    System.out.println(sum);
-    sc.close();
-}
-
 }
